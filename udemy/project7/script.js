5b8714a869358c6c502b45ab3350400c4d9c26d1
Maksim Yudzeshka <yudzeshka@gmail.com>
2021-08-16 17:17:42 +0300

    add project 7 html css

---
+++ udemy/project7/script.js
@@ -0,0 +1,13 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters'); 
+const playAgainBtn = document.getElementById('play-again'); 
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container'); 
+const finalMessage = document.getElementById('final-message');       
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words [Math.floor(Math.random() * word.length)];
+console.log(selectedWord);
