a8cb82ba3c0b1359d14033090ef8a09e51fd0f17
Maksim Yudzeshka <yudzeshka@gmail.com>
2021-09-07 16:47:57 +0300

    finish project 11

--- udemy/project11(speedTypeGame)/script.js
+++ udemy/project11(speedTypeGame)/script.js
@@ -2,7 +2,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game');
+const endgameEl = document.getElementById('end-game-container');
 const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
@@ -43,6 +43,20 @@ let score = 0;
 // Init time 
 let time = 10;
 
+// Set difficulty value in ls or medium
+let difficulty = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
+// Set difficulty select value
+difficulySelect.value = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
+
+// Focus on text on start
+text.focus();
+
+//Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+
 //Generate random word from array
 
 function getRandomWord() {
@@ -62,10 +76,36 @@ function updateScore(){
     scoreEl.innerHTML = score;
 }
 
+// Update time
+function updateTime() {
+time--;
+timeEl.innerHTML = time + 's';
+
+if(time === 0) {
+    clearInterval(timeInterval);
+    //end game
+    gameOver();
+}
+}
+
+// Game over show and screen
+function gameOver() {
+    endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+    `;
+
+    endgameEl.style.display = 'flex';
+
+}
+
 addWordToDOM();
 
 
 // Event Listenrs
+
+// Typing
 
 text.addEventListener('input', e => {
     const insertedText = e.target.value;
@@ -76,5 +116,28 @@ text.addEventListener('input', e => {
 
         // Clear
         e.target.value = '';
+
+        if (difficulty === 'hard') {
+time +=2;
+
+        }
+        else if (difficulty === 'medium') {
+            time +=3;
+
+        }
+        else {
+            time +=5;
+        }
+
+        updateTime()
     }
+})
+
+// Settings btn click
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+// Settings select
+settingsForm.addEventListener('change', e => {
+    difficulty = e.target.value;
+  localStorage.setItem('difficulty', difficulty);
 })
